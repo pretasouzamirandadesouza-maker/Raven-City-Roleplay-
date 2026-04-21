c086ed8c99aafacb9a610c70e6dae75839b8cac7
pretasouzamirandadesouza-maker <pretasouzamirandadesouza@gmail.com>
2026-04-21 01:18:26 -0300

    Launch GTASA activity on button click
    Updated button click listener to start GTASA activity with server details.

--- app/src/main/java/com/samp/mobile/launcher/activity/MainActivity.java
+++ app/src/main/java/com/samp/mobile/launcher/activity/MainActivity.java
@@ -1,12 +1,13 @@
 package com.samp.mobile.launcher.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.samp.mobile.R;
+import com.samp.mobile.game.GTASA;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +21,10 @@ public class MainActivity extends AppCompatActivity {
         btnConnect = findViewById(R.id.btnConnect);
 
         btnConnect.setOnClickListener(v -> {
-            Toast.makeText(this, "Conectar no servidor", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, GTASA.class);
+            intent.putExtra("server_ip", "15.204.150.91");
+            intent.putExtra("server_port", 17930);
+            startActivity(intent);
         });
     }
 }
