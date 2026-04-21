a8008cf25a0e161c990bcb848ed6fb31c97a259e
pretasouzamirandadesouza-maker <pretasouzamirandadesouza@gmail.com>
2026-04-21 00:26:56 -0300

    Replace rgba.h with rRGBA.h in Lines.cpp

--- app/src/main/cpp/samp/game/Lines.cpp
+++ app/src/main/cpp/samp/game/Lines.cpp
@@ -4,7 +4,7 @@
 
 #include "Lines.h"
 #include "../vendor/armhook/patch.h"
-#include "rgba.h"
+#include "rRGBA.h"
 
 void CLines::RenderLineNoClipping(float startX, float startY, float startZ, float endX, float endY, float endZ, uint32 startColor, uint32 endColor) {
     RxObjSpace3DVertex vertices[] = {
